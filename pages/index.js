4bc11e02e216e14079111d9f109559b7d57963ad
dzhenga777 <holnope012@gmail.com>
2025-07-11 14:54:29 +0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,12 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main style={{ fontFamily: 'sans-serif', padding: '2rem' }}>
-      <nav style={{
-        marginBottom: '2rem',
-        borderBottom: '1px solid #ccc',
-        paddingBottom: '1rem',
-        display: 'flex',
-        gap: '1.5rem'
+      <nav style={{ 
+        marginBottom: '2rem', 
+        borderBottom: '1px solid #ccc', 
+        paddingBottom: '1rem', 
+        display: 'flex', 
+        gap: '1.5rem' 
       }}>
         <Link href="/">🏠 Главная</Link>
         <Link href="/about">🧠 Обо мне</Link>
